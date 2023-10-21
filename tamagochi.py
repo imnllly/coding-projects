200ff5bca763ae21583d93479948c1aec9a8d9d5
imnllly <143996748+imnllly@users.noreply.github.com>
2023-10-21 14:30:46 +0300

    tamagochi.py

--- tamagochi.py
+++ tamagochi.py
@@ -25,18 +25,18 @@ def eat():
     progress1.config(value=food)
 
 
-#def music():
-    #global sound
-    #sound -= 1
-    #progress1.config(value=sound)
-    #window.after(1000, music)
+def music():
+    global sound
+    sound -= 1
+    progress2.config(value=sound)
+    window.after(1000, music)
 
-#def nomusic():
-    #global sound
-    #sound += 1
-    #if sound >= 7:
-    #    winsound.PlaySound("minecraftsound.wav", winsound.SND_ASYNC)
-    #progress1.config(value=sound)
+def nomusic():
+    global sound
+    sound += 1
+    if sound >= 7:
+        winsound.PlaySound("minecraftsound.wav", winsound.SND_ASYNC)
+    progress2.config(value=sound)
 
 window = Tk()
 window.title("Тамагочи!")
@@ -44,14 +44,14 @@ fox = PhotoImage(file='fox2.PNG')
 foxsleep = PhotoImage(file='fox1.PNG')
 image_label = Label(window, image=foxsleep)
 image_label.pack()
-progress1 = Progressbar(window, value=sound, maximum=10)
+progress1 = Progressbar(window, value=food, maximum=10)
 progress1.pack()
 button1 = Button(window, text="Покормить", width=10, command=eat)
 button1.pack()
-#progress2 = Progressbar(window, value=food, maximum=10)
-#progress2.pack()
-#button2 = Button(window, text="Включить музыку", width=20, command=music)
-#button2.pack()
+progress2 = Progressbar(window, value=sound, maximum=10)
+progress2.pack()
+button2 = Button(window, text="Включить музыку", width=20, command=music)
+button2.pack()
 
 
 window.after(1000, hunger)
