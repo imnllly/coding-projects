78188267c15bf212e3eb8a6076eaa7b2a1bf796d
imnllly <143996748+imnllly@users.noreply.github.com>
2024-06-03 12:03:10 +0300

    ege27ver2

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-import coder
+# максимальная сумма в массиве
+# j < i
+# l[i] + l[j] -> max
 
-encoded_word = coder.encode('Привет', 52)
-print('Encodede word: ' + encoded_word)
+sp = input().split()
+n = int(sp[0])
+k = int(sp[1])
+l = input().split()
+ibest = 1
+jbest = 0
+imax = 0
 
-secret_number = 4
+for i in range(k+1, len(l)):
+  if l[i - 1] > l[imax]:
+    imax = i - k
+  if l[i] + l[imax] > l[ibest] + l[jbest]:
+    ibest = i
+    jbest = imax
 
-for i in range(1, 9):
-    secret_number += i % 2 * 2
-
-    if secret_number > 7:
-        b = 7
-        while b >= 0:
-            secret_number += int(12 / 4)
-            b -= 1
-
-secret_number /= 2
-secret_number = int(secret_number)
-
-if True:
-    secret_number -= 126 % 100
-
-print('Shift: ' + str(secret_number))
-print('Decodede word: ' + coder.decode(encoded_word, 52))
+print(jbest + 1, ibest + 1)
